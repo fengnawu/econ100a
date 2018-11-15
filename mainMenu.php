89a8c923e07f5c0de448c90d6b0af7a5341b7e09
fengnawu <44347184+fengnawu@users.noreply.github.com>
2018-11-14 19:08:16 -0800

    Add files via upload

---
+++ mainMenu.php
@@ -0,0 +1,8 @@
+<div class="mainMenu">
+  <div style="padding-top: 15px; text-align: right; padding-right: 10px;">
+    <span class="mainMenuTitle"><a <?php echo (basename($page)=='Home'? 'class="active' : '');?> class="mainMenuTitle" href="/~ealdrich/Teaching/Econ133/">ECON 133</a></span>
+    <span class="mainMenu"><a <?php echo (basename($page)=='Teaching'? 'class="active' : '');?> class="mainMenu" href="/~ealdrich/Teaching/Econ133/Syllabus/">Syllabus</a></span>
+    <span class="mainMenu"><a <?php echo (basename($page)=='Teaching'? 'class="active' : '');?> class="mainMenu" href="/~ealdrich/Teaching/Econ133/Slides/">Lecture Notes</a></span>
+    <span class="mainMenu"><a <?php echo (basename($page)=='Research'? 'class="active' : '');?> class="mainMenu" href="/~ealdrich/Teaching/Econ133/">Assignments</a></span>
+  </div>
+</div>
